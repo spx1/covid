601fc1d4d2637dc6655a84094bf79d1afc5559bc
spx1 <smith.wg@gmail.com>
2020-03-30 20:59:45 -0400

    Add Total Display and Relative New Cases

--- covid.py
+++ covid.py
@@ -16,11 +16,18 @@ if __name__ == "__main__":
             if line[1] == 'US':
                 data = [int(x) for x in line[4:]]
 
-    diff=[0]
+    diff=[0.0]
     for x in range(1,len(data)):
-        diff.append(data[x]-data[x-1])
+        diff.append( (data[x]-data[x-1]) / data[x] if data[x] > 0 else 0 )
 
-    pyp.plot(dates,diff)
+    fig,ax1 = pyp.subplots()
+    color = ["tab:red","tab:blue"]
+    ax1.set_xlabel('date')
+    ax1.set_ylabel('Total Cases',color=color[0])
+    ax1.plot(dates,data,color=color[0])
+    ax2 = ax1.twinx()
+    ax2.set_ylabel('Rel New Cases',color=color[1])
+    ax2.plot(dates,diff,color=color[1])
     pyp.show()
 
 
