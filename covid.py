1f23f370e9cf1004e7fdb7169895d56dbd82b37e
spx1 <smith.wg@gmail.com>
2020-03-31 21:31:56 -0400

    Add moving average to diff

--- covid.py
+++ covid.py
@@ -8,6 +8,15 @@ from typing import Tuple,List
 DATA_DIR='covid_data\\csse_covid_19_data\\csse_covid_19_daily_reports'
 DATA_FILE='*.csv'
 
+def moving_average(data: List[int],period: int) -> List[float]:
+    ma = []
+    for i in range(0,min(period,len(data))):
+        ma.append(None)
+
+    for i in range(min(period,len(data)),len(data)):
+        ma.append(sum(data[i:i+period]) / period)   
+    return ma
+
 def get_date_from_filename(path: str) -> datetime.date:
     sd = os.path.basename(path)
     sd = sd[0:sd.rfind(".")]
@@ -58,7 +67,7 @@ if __name__ == "__main__":
     ax1.plot(dates,counts,color=color[0])
     ax2 = ax1.twinx()
     ax2.set_ylabel('Rel New Cases',color=color[1])
-    ax2.plot(dates,rdiffs,color=color[1])
+    ax2.plot(dates,moving_average(rdiffs,3),color=color[1])
     pyp.show()
 
 
