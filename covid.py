0cdf1379be121c9a911c9defc9f7ff713749a1e9
spx1 <smith.wg@gmail.com>
2020-03-31 21:08:54 -0400

    Uses the daily data now

--- covid.py
+++ covid.py
@@ -3,31 +3,62 @@ import os
 from itertools import islice
 import matplotlib.pyplot as pyp
 import datetime
+from typing import Tuple,List
 
-DATA_DIR='covid_data\\csse_covid_19_data\\csse_covid_19_time_series'
-DATA_FILE='time_series_covid19_confirmed_global.csv'
+DATA_DIR='covid_data\\csse_covid_19_data\\csse_covid_19_daily_reports'
+DATA_FILE='*.csv'
+
+def get_date_from_filename(path: str) -> datetime.date:
+    sd = os.path.basename(path)
+    sd = sd[0:sd.rfind(".")]
+    return datetime.datetime.strptime(sd,"%m-%d-%Y")
+
+def process_file(path: str) -> Tuple[datetime.date,int]:
+    def get_cnt_v1(line: List[str],state:str,country:str) -> int:
+        return int(line[3]) if line[0] == state and line[1] == country else 0
+    def get_cnt_v2(line: List[str],state:str,country:str) -> int:
+        return int(line[7]) if line[2] == state and line[3] == country else 0
+
+    with open(path,"tr") as f:
+        df = reader(f,delimiter=',',quotechar='"')
+        for line in islice(df,1):
+            foo_get_cnt = get_cnt_v2 if 'FIPS' in line[0] else get_cnt_v1
+        cnt = 0
+        for line in df:
+            cnt = cnt + foo_get_cnt(line,'New Jersey','US')
+
+    return (get_date_from_filename(path),cnt)
+        
+
 if __name__ == "__main__":
-    with open(os.path.join(DATA_DIR,DATA_FILE)) as f:
-        dr = reader(f,delimiter=',',quotechar='"')
-        for line in islice(dr,1):
-            dates = [datetime.datetime.strptime(x,"%m/%d/%y") for x in line[4:]]
-            #print(','.join(dates))
-        for line in islice(dr,1,None):
-            if line[1] == 'US':
-                data = [int(x) for x in line[4:]]
+    data={}
+    with os.scandir(DATA_DIR) as dir:
+        for entity in dir:
+            if entity.name.lower().endswith('.csv') and entity.is_file():
+                dt,cnt = process_file(entity.path)
+                data[dt]=cnt
 
-    diff=[0.0]
-    for x in range(1,len(data)):
-        diff.append( (data[x]-data[x-1]) / data[x] if data[x] > 0 else 0 )
+    dates = sorted([x for x in data.keys()])
+    counts = []
+    diffs = []
+    for j in dates:
+        counts.append(data[j])
+        ts = datetime.timedelta(days=1)
+        diffs.append( (data[j] - data[j-ts]) if (j-ts) in dates else 0)
+    rdiffs = []
+    for i in range(0,len(counts)):
+        rdiffs.append( diffs[i] / counts[i] if counts[i] > 0 else 0 )
 
+    
+    
     fig,ax1 = pyp.subplots()
     color = ["tab:red","tab:blue"]
     ax1.set_xlabel('date')
     ax1.set_ylabel('Total Cases',color=color[0])
-    ax1.plot(dates,data,color=color[0])
+    ax1.plot(dates,counts,color=color[0])
     ax2 = ax1.twinx()
     ax2.set_ylabel('Rel New Cases',color=color[1])
-    ax2.plot(dates,diff,color=color[1])
+    ax2.plot(dates,rdiffs,color=color[1])
     pyp.show()
 
 
